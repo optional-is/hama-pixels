83892d1a442d9dc7ea645ac58195e91508701805
brian suda <brian@suda.co.uk>
2013-08-09 14:47:55 +0000

    updating colors

--- hama_colors.php
+++ hama_colors.php
@@ -4,37 +4,37 @@
 // Depending on the brand, you can adjust these values accordingly
 
 return array(
-	array('name'=>'Green'          ,'red'=>85, 'green'=>187,'blue'=>81),   /* Green         207-10 */
-	array('name'=>'Purple'         ,'red'=>83, 'green'=>42, 'blue'=>92),   /* Purple        207-07 */
-	array('name'=>'Light Brown'    ,'red'=>141,'green'=>76, 'blue'=>48),   /* Light Brown   207-21 */
-	array('name'=>'Dark Green'     ,'red'=>85, 'green'=>187,'blue'=>81),   /* Dark Green    207-28  Olive */
-	array('name'=>'Orange'         ,'red'=>185,'green'=>59, 'blue'=>44),   /* Orange        207-04*/
-	array('name'=>'Yellow'         ,'red'=>212,'green'=>168,'blue'=>58),   /* Yellow        207-03 */
-	array('name'=>'Grey'           ,'red'=>190,'green'=>165,'blue'=>134),  /* Grey          207-17 */
-	array('name'=>'Light Blue'     ,'red'=>33, 'green'=>100,'blue'=>202),  /* Light Blue    207-09 */
-	array('name'=>'Brown'          ,'red'=>156,'green'=>78, 'blue'=>31),   /* Brown         207-12 */
-	array('name'=>'Reddish Brown'  ,'red'=>228,'green'=>72, 'blue'=>40),   /* Reddish Brown 207-20 */
-	array('name'=>'Pastel Blue'    ,'red'=>110,'green'=>208,'blue'=>244),  /* Pastel Blue   207-46 */
-	array('name'=>'Blue'           ,'red'=>10, 'green'=>24, 'blue'=>99),   /* Blue          207-08 */
-	array('name'=>'Cream'          ,'red'=>252,'green'=>246,'blue'=>182),  /* Cream         207-02 */
-	array('name'=>'Beige'          ,'red'=>192,'green'=>144,'blue'=>89),   /* Beige         207-27 */
-	array('name'=>'White'          ,'red'=>205,'green'=>206,'blue'=>207),  /* White         207-01 */
-	array('name'=>'Turquoise'      ,'red'=>80, 'green'=>129,'blue'=>138),  /* Turquoise     207-31 */
-	array('name'=>'Red'            ,'red'=>153,'green'=>45, 'blue'=>55),   /* Red           207-05 */
+	array('name'=>'Green'          ,'red'=>0,  'green'=>78, 'blue'=>59),   /* Green         207-10 */
+	array('name'=>'Purple'         ,'red'=>56, 'green'=>9,  'blue'=>90),   /* Purple        207-07 */
+	array('name'=>'Light Brown'    ,'red'=>131,'green'=>55, 'blue'=>25),   /* Light Brown   207-21 */
+	array('name'=>'Dark Green'     ,'red'=>36, 'green'=>42, 'blue'=>46),   /* Dark Green    207-28  Olive */
+	array('name'=>'Orange'         ,'red'=>171,'green'=>1,  'blue'=>1),    /* Orange        207-04*/
+	array('name'=>'Yellow'         ,'red'=>187,'green'=>149,'blue'=>8),    /* Yellow        207-03 */
+	array('name'=>'Grey'           ,'red'=>67, 'green'=>75, 'blue'=>93),   /* Grey          207-17 */
+	array('name'=>'Light Blue'     ,'red'=>28, 'green'=>27, 'blue'=>139),  /* Light Blue    207-09 */
+	array('name'=>'Brown'          ,'red'=>35, 'green'=>23, 'blue'=>26),   /* Brown         207-12 */
+	array('name'=>'Reddish Brown'  ,'red'=>113,'green'=>17, 'blue'=>13),   /* Reddish Brown 207-20 */
+	array('name'=>'Pastel Blue'    ,'red'=>18, 'green'=>105,'blue'=>175),  /* Pastel Blue   207-46 */
+	array('name'=>'Blue'           ,'red'=>17, 'green'=>0,  'blue'=>84),   /* Blue          207-08 */
+	array('name'=>'Cream'          ,'red'=>185,'green'=>186,'blue'=>158),  /* Cream         207-02 */
+	array('name'=>'Beige'          ,'red'=>191,'green'=>151,'blue'=>119),  /* Beige         207-27 */
+	array('name'=>'White'          ,'red'=>196,'green'=>198,'blue'=>210),  /* White         207-01 */
+	array('name'=>'Turquoise'      ,'red'=>27, 'green'=>115,'blue'=>147),  /* Turquoise     207-31 */
+	array('name'=>'Red'            ,'red'=>162,'green'=>2,  'blue'=>20),   /* Red           207-05 */
 	array('name'=>'Light Green'    ,'red'=>75, 'green'=>171,'blue'=>131),  /* Light Green   207-11 */
-	array('name'=>'Black'          ,'red'=>24, 'green'=>26, 'blue'=>29),   /* Black         207-18 */
-	array('name'=>'Pink'           ,'red'=>202,'green'=>112,'blue'=>123),  /* Pink          207-06 */
-	array('name'=>'Flesh'          ,'red'=>197,'green'=>129,'blue'=>111),  /* Flesh         207-26 */
-	array('name'=>'Burgundy'       ,'red'=>92 ,'green'=>50, 'blue'=>58),   /* Burgundy      207-20 */
-	array('name'=>'Pastel Purple'  ,'red'=>125,'green'=>97 ,'blue'=>149), /* Pastel Purple  207-45 */
-	array('name'=>'Pastel Pink'    ,'red'=>182,'green'=>103,'blue'=>149), /* Pastel Pink    207-48 */
-	array('name'=>'Claret'         ,'red'=>157,'green'=>29 ,'blue'=>56),   /* Claret        207-29 */
+	array('name'=>'Black'          ,'red'=>11, 'green'=>12, 'blue'=>16),   /* Black         207-18 */
+	array('name'=>'Pink'           ,'red'=>209,'green'=>102,'blue'=>132),  /* Pink          207-06 */
+	array('name'=>'Flesh'          ,'red'=>178,'green'=>104,'blue'=>101),  /* Flesh         207-26 */
+	array('name'=>'Burgundy'       ,'red'=>44 ,'green'=>12, 'blue'=>27),   /* Burgundy      207-20 */
+	array('name'=>'Pastel Purple'  ,'red'=>95, 'green'=>40 ,'blue'=>141),  /* Pastel Purple  207-45 */
+	array('name'=>'Pastel Pink'    ,'red'=>186,'green'=>79 ,'blue'=>160),  /* Pastel Pink    207-48 */
+	array('name'=>'Claret'         ,'red'=>125,'green'=>0  ,'blue'=>33),   /* Claret        207-29 */
  
 	
 	array('name'=>'Dark Red'       ,'red'=>130,'green'=>23, 'blue'=>34),  /* Dark Red */
 	array('name'=>'Pastel Yellow'  ,'red'=>225,'green'=>204,'blue'=>60),  /* Pastel Yellow */
 	array('name'=>'Pastel Red'     ,'red'=>197,'green'=>75, 'blue'=>71),  /* Pastel Red */
-	array('name'=>'Pastel Green'   ,'red'=>149,'green'=>186,'blue'=>100), /* Pastel Green */
+	array('name'=>'Pastel Green'   ,'red'=>0,'green'=>125,'blue'=>107), /* Pastel Green */
 	array('name'=>'Azure'          ,'red'=>88, 'green'=>168,'blue'=>188), /* Azure */
 	array('name'=>'Teddybear Brown','red'=>215,'green'=>151,'blue'=>45),  /* Teddybear Brown */
 	
